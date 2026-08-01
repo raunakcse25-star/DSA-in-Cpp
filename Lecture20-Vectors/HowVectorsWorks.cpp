5d247d2535bad6eaf188997d0326de25e5201769
Raunak <raunaksahni7286@gmail.com>
2026-08-01 16:17:52 +0530

    how vectors work

---
+++ Lecture20-Vectors/HowVectorsWorks.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(4);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
+    for (int i=0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout<<endl;
+}
