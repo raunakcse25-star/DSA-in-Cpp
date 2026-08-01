7188a0a3e68b0d94b23a361d58c9ae433a7949ac
Raunak <raunaksahni7286@gmail.com>
2026-08-01 16:35:30 +0530

    how vectors work

--- Lecture20-Vectors/HowVectorsWorks.cpp
+++ Lecture20-Vectors/HowVectorsWorks.cpp
@@ -7,12 +7,29 @@ int main()
 {
     vector<int> v;
     v.push_back(4);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
     v.push_back(1);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
     v.push_back(2);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
     v.push_back(4);
-    v.push_back(3);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
+    v.push_back(9);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
+    v.pop_back();
+    cout<<v.size()<<v.capacity()<<" "<<endl;
+    v.pop_back();
+    cout<<v.size()<<v.capacity()<<" "<<endl;
     v.push_back(6);
-    v.push_back(7);
+    cout<<v.size()<<v.capacity()<<" "<<endl;
+
+    // v.push_back(4);
+    // v.push_back(1);
+    // v.push_back(2);
+    // v.push_back(4);
+    // v.push_back(3);
+    // v.push_back(6);
+    // v.push_back(7);
     for (int i=0; i < v.size(); i++)
     {
         cout << v[i] << " ";
