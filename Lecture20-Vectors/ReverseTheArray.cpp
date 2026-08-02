90775131f7cbdf87d4e2b11cb1f8001d1370f63b
Raunak <raunaksahni7286@gmail.com>
2026-08-02 16:45:27 +0530

    reverseTheArray

--- Lecture20-Vectors/ReverseTheArray.cpp
+++ Lecture20-Vectors/ReverseTheArray.cpp
@@ -3,10 +3,25 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main(){
-    vector <int> v = {10,20,30,40,50,60,70};
-    reverse(v.begin(),v.end());
-    for(int elem :v)
-    cout<<elem<<" ";
-
+void print(vector<int> &arr){
+    for(int elem: arr) cout<<elem<<" ";
+    cout<<endl;
 }
+
+int main(){
+    vector <int> arr = {10,20,30,40,50,60,70};
+    int i =0;
+    int j =arr.size() -1;
+    print(arr);
+    while(i<j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+    }
+    print(arr);
+}
+
+
+//  TC   ==> O(n)
