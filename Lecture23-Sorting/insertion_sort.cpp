838557a49c3b1aa6287e241f0e4d0d592d23ec00
Raunak Sahni <raunaksahni7286@gmail.com>
2026-08-16 16:50:40 +0530

    insertion sort

---
+++ Lecture23-Sorting/insertion_sort.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int>& arr) {
+    for(int x : arr) cout << x << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {5,4,3,6,2,1};
+    int n = arr.size();
+    print(arr);
+
+    // Insertion Sort
+    for(int i = 1; i <= n - 1; i++) {
+        int j = i;
+        while(j >= 1 && arr[j] < arr[j - 1]) {
+            swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
+
+    print(arr);
+    return 0;
+}
